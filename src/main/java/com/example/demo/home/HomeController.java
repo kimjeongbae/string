53fec08fe6f408c517c1a6690d189c855ce4f3ba
kimjeongbae <gprhf651@naver.com>
2023-12-26 16:00:29 +0900

    delete

---
+++ src/main/java/com/example/demo/home/HomeController.java
@@ -0,0 +1,64 @@
+package com.example.demo.home;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HomeController {
+    List<Person> personList = new ArrayList<>();
+    public int lastid = 1;
+    public int i = 1;
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public int index() {
+        return i++;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public int plus(@RequestParam(value = "a", defaultValue = "0") int a, @RequestParam(value = "b") int b) {
+        return a + b;
+    }
+
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String addPerson(@RequestParam(value = "name") String name, @RequestParam(value = "age") int age) {
+        int id = lastid;
+        Person person = new Person(lastid, name, age);
+        personList.add(person);
+
+        lastid++;
+
+        return String.format("%d 번째 사람이 추가 되었습니다.", id);
+    }
+
+    @GetMapping("/home/removePerson")
+    @ResponseBody
+    public String removePerson(@RequestParam("id") int id){
+        boolean isRemove = personList.removeIf(person -> person.getId() == id);
+
+        if(!isRemove){
+            return
+        }
+            return
+
+    }
+
+
+    @GetMapping("/home/people")
+    @ResponseBody
+    public List<Person> people () {
+
+        return personList;
+    }
+
+}
+
---
+++ src/main/java/com/example/demo/home/Person.java
@@ -0,0 +1,17 @@
+package com.example.demo.home;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Person {
+    int id;
+    String name;
+    int age;
+}
